df91cf9e2c04ea192334a1ed37d50a6b22d89005
Couper12 <coupersemail@gmail.com>
2018-06-04 10:17:45 -0400

    Create MainWindow.xaml.cs

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+/* 
+ * Couper Ebbs-Picken
+ * 5/4/2018
+ * Do more math
+ */ 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace u5_Printing_CouperEbbsPicken
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int copies = 0;
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+        }
+
+        private void btnRun_Click(object sender, RoutedEventArgs e)
+        {
+            lblPricePerCopy.Content = "Price per copys is $";
+            lblTotalPrice.Content = "Total cost is: $";
+            double price;
+            int.TryParse(txtInput.Text, out copies);
+            if (copies <= 99)
+            {
+                price = 0.30;
+                lblPricePerCopy.Content += price.ToString();
+                lblTotalPrice.Content += Math.Round(price * copies, 2).ToString();
+            }
+
+            if (copies <= 499
+                && copies >= 100)
+            {
+                price = 0.28;
+                lblPricePerCopy.Content += price.ToString();
+                lblTotalPrice.Content += Math.Round(price * copies, 2).ToString();
+            }
+
+            if (copies <= 749
+                && copies >= 500)
+            {
+                price = 0.27;
+                lblPricePerCopy.Content += price.ToString();
+                lblTotalPrice.Content += Math.Round(price * copies, 2).ToString();
+            }
+
+            if (copies <= 1000
+                && copies >= 750)
+            {
+                price = 0.26;
+                lblPricePerCopy.Content += price.ToString();
+                lblTotalPrice.Content += Math.Round(price * copies, 2).ToString();
+            }
+
+            if (copies > 1000)
+            {
+                price = 0.25;
+                lblPricePerCopy.Content += price.ToString();
+                lblTotalPrice.Content += Math.Round(price * copies, 2).ToString();
+            }
+        }
+    }
+}
